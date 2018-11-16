2365bc7514d56db82dc4dc16d75242e3748ef433
Stefan Kraatz <Stefan.Kraatz@b-tu.de>
2018-11-16 14:44:36 +0100

    started implementing k-means alghorithm

--- assignment1/script.py
+++ assignment1/script.py
@@ -3,6 +3,7 @@
 import sys
 import math
 import numpy
+import random
 from PIL import Image
 
 image_dimensions = (0,0)
@@ -30,28 +31,45 @@ def getTupel(image, x, y):
 
 # distance function, takes two rgb tupels
 def eukliddist(pointA,pointB):
-    mindim=min(len(pointA),len(pointB))
-    sum=0
-    for i in range(mindim):
-        sum+=math.pow(pointA[i]-pointB[i],2)
-    return math.sqrt(sum)
+    r_a, g_a, b_a = pointA
+    (r_b, g_b, b_b), clid = pointB
+    
+    sum=math.pow(r_a-r_b,2)
+    sum+=math.pow(g_a-g_b,2)
+    sum+=math.pow(b_a-b_b,2)
+    return (math.sqrt(sum), clid)
 
 def set_cluster_id(x, y, id):
     cl[x,y] = id
 
+def get_cluster_id(x,y):
+    return cl[x,y]
 
-def k_means(image):
+def k_means(image, k):
     # chose k points as center
-    #centers=list()
-    #bool changed = True
-	#while changed:
-    #    pass
-        # for each center in centers
-		    # for each point in image
-                # calculate dist(point, center)
-				# set cluster_id as closest cluster
-				#
-    pass
+    x_w, y_w = image_dimensions
+    # centers should be tupels of (RGB tupel, cluster_id)
+    centers=list()
+    for clid in range(0, k):
+        # get random point? may result in equal color points
+        x_r = random.randint(0,x_w)
+        y_r = random.randint(0,y_w)
+        p = getTupel(image, x_r, y_r)
+        centers.append((p,k))
+    changed = True
+    while changed:
+        changed = False
+        # iterate over all points
+        for x in range(0, x_w ):
+            for y in range(0, y_w ):
+                d_min = float("inf")
+                current_cluster = get_cluster_id(x,y)
+                # compute distance to all centers
+                for center in centers:
+                    d, clid = eukliddist(getTupel(image, x, y), center)
+                    if d<d_min:
+                        d_min = d
+			# set cluster_id as closest cluster
 
 
 # main program
@@ -63,5 +81,6 @@ if __name__ == "__main__":
     print(image_dimensions)
     set_cluster_id(224,282,1)
     print(cl)
+    k_means(im, 4)
 
 
